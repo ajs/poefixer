1bc6f649208177ccc23738d893ef877550774326
Aaron Sherman <ajs@ajs.com>
2018-08-02 17:08:03 -0400

    Parameterized DB credentials

--- scripts/simplereader.py
+++ scripts/simplereader.py
@@ -5,18 +5,37 @@ Trivial API reader/writer for testing
 """
 
 
+import argparse
 import poefixer
 
 
-db = poefixer.PoeDb(db_connect='sqlite:///:memory:')
-api = poefixer.PoeApi()
+DEFAULT_DSN='sqlite:///:memory:'
 
-db.create_database()
 
-while True:
-    for stash in api.get_next():
-        print("Inserting stash...")
-        db.insert_api_stash(stash, with_items=True)
-    db.session.commit()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d', '--database-dsn', action='store',
+        default=DEFAULT_DSN,
+        help='Database connection string for SQLAlchemy')
+    return parser.parse_args()
 
-# vim: sw=4 sts=4 ai:
+def pull_data(database_dsn):
+    db = poefixer.PoeDb(db_connect=database_dsn)
+    api = poefixer.PoeApi()
+
+    db.create_database()
+
+    while True:
+        for stash in api.get_next():
+            print("Inserting stash...")
+            db.insert_api_stash(stash, with_items=True)
+        db.session.commit()
+
+
+if __name__ == '__main__':
+    options = parse_args()
+    pull_data(database_dsn=options.database_dsn)
+
+
+# vim: et:sw=4:sts=4:ai:

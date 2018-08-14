4a2f3aadac17369964cd589eecc74df01bcd84b8
Aaron Sherman <ajs@ajs.com>
2018-08-14 18:20:48 -0400

    Additional testing for currency conversion prices

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -12,6 +12,49 @@ from poefixer.extra.sample_data import sample_stash_data
 from poefixer.postprocess.currency import CurrencyPostprocessor
 
 
+class CurrencyStep:
+    """A linked list of currency conversion steps with prices"""
+
+    currency = None
+    price = None
+    _price_float = None
+    next_step = None
+
+    def __init__(self, currency, price=None, next_step=None):
+        self.currency = currency
+        self.price = price
+        self.next_step = next_step
+
+        if price:
+            if isinstance(price, str):
+                if '/' in price:
+                    num, den = price.split('/')
+                    self._price_float = float(num)/float(den)
+                    return
+            self._price_float = float(price)
+
+    def _get_conversion_steps(self):
+        if self.price:
+            assert self.next_step, "If a price is set, must have next_step"
+            yield (self.currency, self.next_step.currency, self.price)
+            for sample in self.next_step._get_conversion_steps():
+                yield sample
+
+    def get_sample_stashes(self):
+        return sample_stashes(list(self._get_conversion_steps()))
+
+    def conversion_price(self):
+        price = self._price_float
+        if self.next_step and self.next_step.price:
+            price *= self.next_step.conversion_price()
+        return price
+
+    def conversion_goal(self):
+        if self.next_step:
+            return self.next_step.conversion_goal()
+        return self.currency
+
+
 class TestPoefixerDb(unittest.TestCase):
 
     DB_URI = 'sqlite:///:memory:'
@@ -24,6 +67,10 @@ class TestPoefixerDb(unittest.TestCase):
         db.create_database()
         return db
 
+    def _currency_postprocessor(self, db, recent=None):
+        return CurrencyPostprocessor(
+            db, start_time=None, recent=recent, logger=self.logger)
+
     def test_insert_currency(self):
         """
         Test the currency summary handling on a list of
@@ -33,9 +80,9 @@ class TestPoefixerDb(unittest.TestCase):
         in line with the majority of results.
         """
 
-        stashes = self._sample_stashes(
-            ([("Chaos Orb", "Exalted Orb", 0.01) for _ in range(20)] +
-             [("Chaos Orb", "Exalted Orb", 100)]))
+        stashes = sample_stashes(
+            [("Chaos Orb", "Exalted Orb", 0.01) for _ in range(20)] +
+            [("Chaos Orb", "Exalted Orb", 100)])
 
         db = self._get_default_db()
 
@@ -43,12 +90,13 @@ class TestPoefixerDb(unittest.TestCase):
             db.insert_api_stash(stash, with_items=True)
         db.session.commit()
 
-        cp = CurrencyPostprocessor(db, None, logger=self.logger)
+        cp = self._currency_postprocessor(db)
         cp.do_currency_postprocessor()
 
         query = db.session.query(poefixer.CurrencySummary)
         self.assertEqual(query.count(), 1)
-        row = query.one()
+        row = query.one_or_none()
+        self.assertIsNotNone(row)
         self.assertEqual(row.from_currency, "Chaos Orb")
         self.assertEqual(row.to_currency, "Exalted Orb")
         self.assertEqual(row.count, 20) # The extreme value was discarded
@@ -62,18 +110,11 @@ class TestPoefixerDb(unittest.TestCase):
         """
 
         from_c = "My Precious"
-        stashes = self._sample_stashes([(from_c, "Exalted Orb", 0.01)])
+        stashes = sample_stashes([(from_c, "Exalted Orb", 0.01)])
 
         db = self._get_default_db()
 
-        for stash in stashes:
-            db.insert_api_stash(stash, with_items=True)
-        db.session.commit()
-
-        cp = CurrencyPostprocessor(db, None, logger=self.logger)
-        cp.do_currency_postprocessor()
-        # Second time picks up the new names we've seen
-        cp.do_currency_postprocessor()
+        cp = self._currency_processor_harness(db, stashes)
 
         # Now see if we'll use those new names
         (amt, cur) = cp.parse_note("~price 1 " + from_c)
@@ -85,6 +126,62 @@ class TestPoefixerDb(unittest.TestCase):
         self.assertEqual(amt, 1)
         # Make sure it goes back to the original
         self.assertEqual(cur, from_c)
+
+    def _currency_processor_harness(self, db, stashes):
+        for stash in stashes:
+            db.insert_api_stash(stash, with_items=True)
+        db.session.commit()
+
+        cp = self._currency_postprocessor(db)
+        cp.do_currency_postprocessor()
+        # Second time picks up the new names we've seen
+        cp.do_currency_postprocessor()
+
+        return cp
+
+    def _currency_valuation_check(self, conversion, alt_conversions=None):
+        """Test a conversion price (conversion is a CurrencyStep list)"""
+
+        stashes = conversion.get_sample_stashes()
+        if alt_conversions:
+            for alt_conv in alt_conversions:
+                stashes += alt_conv.get_sample_stashes()
+
+        db = self._get_default_db()
+        cp = self._currency_processor_harness(db, stashes)
+        # Repeat in order to re-add each currency, forcing
+        # connections to be made.
+        cp = self._currency_processor_harness(db, stashes)
+
+        from_currency = conversion.currency
+        to_currency = conversion.conversion_goal()
+        self.assertEqual(
+            to_currency, "Chaos Orb",
+            "valuation test requires Chaos Orb target")
+        price = conversion.conversion_price()
+
+        self.assertIsNotNone(price)
+
+        cp_price = cp.find_value_of(from_currency, "Standard", 1)
+
+        self.assertIsNotNone(
+            cp_price,
+            "Conversion from %s->chaos" % from_currency)
+        self.assertAlmostEqual(price, cp_price)
+
+    def test_exalt_for_chaos(self):
+        """Test pricing of ex -> chaos"""
+
+        self._currency_valuation_check(
+            CurrencyStep("Exalted Orb", 100, CurrencyStep("Chaos Orb")))
+
+    def text_exalt_for_chrom_for_chaos(self):
+        """Test price of ex -> chrom -> chaos"""
+
+        self._currency_valuation_check(
+            CurrencyStep(
+                "Exalted Orb", 500, CurrencyStep(
+                    "Chromatic Orb", "1/5", CurrencyStep("Chaos Orb"))))
 
     def test_currency_abbreviations(self, single=None, should_be=None):
         """
@@ -112,7 +209,7 @@ class TestPoefixerDb(unittest.TestCase):
                 "fus", "alchemy", "gemc", "ex")
 
         db = self._get_default_db()
-        cp = CurrencyPostprocessor(db, None, logger=self.logger)
+        cp = self._currency_postprocessor(db)
 
         for currency in currency_abbrevs:
             (amt, cur) = cp.parse_note("~price 1 " + currency)
@@ -154,24 +251,25 @@ class TestPoefixerDb(unittest.TestCase):
         self.test_currency_abbreviations(
             single='orb-of-anullment', should_be='Orb of Annulment')
 
-    def _sample_stashes(self, descriptors):
 
-        stash = {
-            'id': '%064x' % 123456,
-            'accountName': 'JoeTest',
-            'stash': 'Goodies',
-            'stashType': 'X',
-            'public': True,
-            'league': 'Standard',
-            'items': []}
+def sample_stashes(descriptors):
 
-        offset = 0
-        for desc in descriptors:
-            (from_c, to_c, price) = desc
-            stash['items'].append(currency_item(from_c, offset, to_c, price))
-            offset += 1
+    stash = {
+        'id': '%064x' % 123456,
+        'accountName': 'JoeTest',
+        'stash': 'Goodies',
+        'stashType': 'X',
+        'public': True,
+        'league': 'Standard',
+        'items': []}
 
-        return [poefixer.ApiStash(stash)]
+    offset = 0
+    for desc in descriptors:
+        (from_c, to_c, price) = desc
+        stash['items'].append(currency_item(from_c, offset, to_c, price))
+        offset += 1
+
+    return [poefixer.ApiStash(stash)]
 
 def currency_item(currency, offset, ask_currency, ask_value):
     """
